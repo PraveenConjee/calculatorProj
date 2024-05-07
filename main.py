56bc2008d308008577f4a31abac229d6f412b06d
PraveenConjee <Praveenc2105@gmail.com>
2024-05-06 23:48:46 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+first_number = int(input("Enter start year: "))
+second_number = int(input("Enter end year: "))
+
+add = first_number+second_number
+sub = first_number-second_number
+mult = first_number*second_number
+div = first_number/second_number
+
+print(first_number, "+", second_number, "=", add)
+print(first_number, "-", second_number, "=", sub)
+print(first_number, "*", second_number, "=", mult)
+print(first_number, "/", second_number, "=", div)
